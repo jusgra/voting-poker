dda199478d096675cad9246ad7cd63898094018f
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2024-10-26 23:13:08 +0300

    unnecessary delete

--- server/expressRouting.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-
-export const expressRouting = (app) => {
-  app.get("/create-room", (req, res) => {
-    const roomId2 = uuidv4();
-    const roomId = "123";
-    res.redirect(`http://localhost:3000/room/${roomId}`);
-  });
-};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import { createServer } from "node:http";
 import cors from "cors";
-import { expressRouting } from "./expressRouting.js";
 import { socketIoLogic } from "./socketIoLogic.js";
 
 const app = express();
 app.use(cors());
 const server = createServer(app);
 
-expressRouting(app);
 socketIoLogic(server);
 
 server.listen(3001, () => {
